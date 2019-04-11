e573978799d664b912cb0313b5d664e33bcecd45
juniorsilvacc <43589505+juniorsilvacc@users.noreply.github.com>
2019-04-11 16:58:07 -0300

    Create Tabuada.java

---
+++ Tabuada.java
@@ -0,0 +1,16 @@
+package  exer1 ;
+
+import  java.util.Scanner ;
+
+ classe  pública Tabuada {
+	public  static  void  main ( String [] args ) {
+		float num;
+		int i;
+		Digitalização do scanner =  novo  Scanner ( System . In);
+		Sistema . fora . println ( " informe um número não qualificado ver uma tabuada: " );
+		num = scan . nextFloat ();
+		para (i =  1 ; i <=  10 ; i ++ ) {
+			Sistema . fora . println (num +  " * "  + i +  " = "  + (num * i));
+		}
+	}
+}
