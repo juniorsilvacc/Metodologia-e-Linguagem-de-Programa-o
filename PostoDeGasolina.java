49071c099936b544f87908e57aeb5afbe0a58cb3
juniorsilvacc <43589505+juniorsilvacc@users.noreply.github.com>
2019-04-11 16:56:02 -0300

    Create PostoDeGasolina.java

---
+++ PostoDeGasolina.java
@@ -0,0 +1,14 @@
+package  exer1 ;
+
+import  java.util.Scanner ;
+
+ classe  pública PostoDeGasolina {
+	public  static  void  main ( String [] args ) {
+		Digitalização do scanner =  novo  Scanner ( System . In);
+		float litros;
+		Sistema . fora . println ( " caldeo de caldeiristas licorosos abastecidos " );
+		litros = scan . nextFloat ();
+		// 2 refrigerantes é 6 reais
+		Sistema . fora . println ( " Valor total: "  + ( 6  + litros *  2.5 ));
+	}
+}
